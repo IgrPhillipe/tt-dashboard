c113a38dd8b7619e5b1d423ccc81fc1cc301a879
igrphillipe <igrphillipe@gmail.com>
2021-03-24 23:29:24 -0300

    feat(project-structure): create styles index

---
+++ src/assets/themes/index.ts
@@ -0,0 +1,34 @@
+export default {
+  mainColors: {
+    white: '#FFFF',
+    black: '#000',
+    darkGray: '#333333',
+    mediumGray: '#C4C4C4',
+    lightGray: '#FAFAFA',
+    green: '#7FC008',
+  },
+  fonts: {
+    h1: {
+      size: '25px',
+    },
+    title: {
+      size: '14px',
+    },
+    subtitle: {
+      size: '12px',
+    },
+    legend: {
+      size: '10px',
+    },
+  },
+  margins: {
+    top: '15px',
+    bottom: '15px',
+    left: '15px',
+    right: '15px',
+  },
+  borderRadius: {
+    input: '100px',
+    card: '5px',
+  },
+};
