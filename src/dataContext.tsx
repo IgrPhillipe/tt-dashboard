ce26e96b08fe2388b91c6c6a1e3cb6145f42200e
igrphillipe <igrphillipe@gmail.com>
2021-03-28 17:49:38 -0300

    feat(home): create dataContext functions

--- src/dataContext.tsx
+++ src/dataContext.tsx
@@ -1,36 +1,93 @@
 import React, { useState, createContext } from 'react';
 
-import { inspections } from './data.json';
+import { inspections, plans } from './data.json';
 
 export const DataContext = createContext({});
 
 export const DataProvider = (props: any) => {
+  const [inspectionsPlanned, setInspectionsPlanned] = useState(0);
+  const [inspectionsRealized, setInspectionsRealized] = useState(0);
+  const [inspectionsCounter, setInspectionsCounter] = useState({});
+  const [plansCounter, setPlansCounter] = useState({});
+  const [plansOpened, setPlansOpened] = useState(0);
+  const [plansClosed, setPLansClosed] = useState(0);
+
   let countPlanned = 0;
   let countRealized = 0;
+  let countOpened = 0;
+  let countClosed = 0;
   let monday = 0;
   let tuesday = 0;
   let wednesday = 0;
   let thursday = 0;
   let sunday = 0;
 
-  inspections.forEach((person) => {
-    person.inspections.forEach((inspection) => {
-      const date = inspection.last_update.split(' ');
+  const Inspections = () => {
+    inspections.forEach((person) => {
+      person.inspections.forEach((inspection) => {
+        const date = inspection.last_update.split(' ');
+
+        switch (date[0]) {
+          case 'Mon': {
+            monday += 1;
+            break;
+          }
+          case 'Tue': {
+            tuesday += 1;
+            break;
+          }
+          case 'Wed': {
+            wednesday += 1;
+            break;
+          }
+          case 'Thu': {
+            thursday += 1;
+            break;
+          }
+          default: {
+            sunday += 1;
+            break;
+          }
+        }
+
+        if (inspection.planned === true) {
+          countPlanned += 1;
+        }
+        if (inspection.realized === true) {
+          countRealized += 1;
+        }
+      });
+    });
+
+    return {
+      monday: monday,
+      tuesday: tuesday,
+      wednesday: wednesday,
+      thursday: thursday,
+      sunday: sunday
+    };
+  };
+
+  const totalInspections = Inspections();
+
+  const Plans = () => {
+    plans.forEach((plan) => {
+      const date = plan.Date.split('/');
 
       switch (date[0]) {
-        case 'Mon': {
+        case '22': {
           monday += 1;
           break;
         }
-        case 'Tue': {
+        case '23': {
           tuesday += 1;
           break;
         }
-        case 'Wed': {
+        case '24': {
           wednesday += 1;
           break;
         }
-        case 'Thu': {
+        case '25': {
           thursday += 1;
           break;
         }
@@ -40,25 +97,36 @@ export const DataProvider = (props: any) => {
         }
       }
 
-      if (inspection.planned === true) {
-        countPlanned += 1;
-      }
-      if (inspection.realized === true) {
-        countRealized += 1;
+      if (plan.status === 'PENDENTE') {
+        countClosed += 1;
+      } else {
+        countOpened += 1;
       }
     });
-  });
 
-  const GlobalData = {
-    data: inspections,
-    planned: countPlanned,
-    realized: countRealized,
-    inspections: {
+    return {
       monday: monday,
       tuesday: tuesday,
       wednesday: wednesday,
       thursday: thursday,
       sunday: sunday
+    };
+  };
+
+  const totalPlans = Plans();
+
+  const GlobalData = {
+    inspections: {
+      data: inspections,
+      planned: countPlanned,
+      realized: countRealized,
+      total: totalInspections
+    },
+    plans: {
+      data: plans,
+      open: countOpened,
+      closed: countClosed,
+      total: totalPlans
     }
   };
 
