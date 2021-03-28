49cc411a822fae7f10843c651d273b88ef879ddf
igrphillipe <igrphillipe@gmail.com>
2021-03-28 18:30:33 -0300

    feat(home): create DailyInspections chart component

---
+++ src/components/Inspections/DailyInspections/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Chart } from 'react-google-charts';
+
+import { DataContext } from '../../../dataContext';
+
+import Header from '../../Header/index';
+
+import {
+  Card, ChartBox,
+} from './style';
+
+const DailyInspections: React.FC = () => {
+  const { GlobalData: Data }: any = React.useContext(DataContext);
+
+  return (
+    <>
+      <Card width='small' height='small' >
+        <Header settings title='Inspeções' subtitle='Status do dia' />
+        <ChartBox>
+          <Chart
+            width={'335px'}
+            height={'250px'}
+            chartType='PieChart'
+            loader={<div>Loading</div>}
+            data={[
+              ['', '', ''],
+              ['Realizados', Data.inspections.realized, 0],
+              ['Não realizados', (Data.inspections.planned - Data.inspections.realized), 0],
+              ['Total', Data.inspections.planned, 0],
+            ]}
+          />
+
+        </ChartBox>
+      </Card>
+    </>
+  );
+
+};
+
+export default DailyInspections;
